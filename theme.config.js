614d4cf12ea575077afa2e83d42bdec27415d41a
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-03-05 08:10:25 +0530

    weee

--- theme.config.js
+++ theme.config.js
@@ -3,8 +3,8 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
-      <a href="/feed.xml">RSS</a>
+      <time>{YEAR}</time> © Nisarga Adhikary.
+     
       <style jsx>{`
         a {
           float: right;
