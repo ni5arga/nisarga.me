9793e6ee6bdcc0b37721760299c1ae4f56659a24
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-03-06 08:05:36 +0530

    added source code

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Nisarga Adhikary.
-     
+     <a href="https://github.com/ni5arga/nisarga.me/">Source Code</a>
       <style jsx>{`
         a {
           float: right;
