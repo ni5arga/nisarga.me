35db2410d5c696f850505cabeef979168d1ab88c
Nisarga Adhikary <git@nisarga.me>
2023-03-23 10:51:50 +0530

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -10,20 +10,20 @@ async function generate() {
     feed_url: 'https://nisarga.me/feed.xml'
   })
 
-  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
+  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'blog'))
   const allPosts = []
   await Promise.all(
     posts.map(async (name) => {
       if (name.startsWith('index.')) return
 
       const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
+        path.join(__dirname, '..', 'pages', 'blog', name)
       )
       const frontmatter = matter(content)
 
       allPosts.push({
         title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
+        url: '/blog/' + name.replace(/\.mdx?/, ''),
         date: frontmatter.data.date,
         description: frontmatter.data.description,
         categories: frontmatter.data.tag.split(', '),
