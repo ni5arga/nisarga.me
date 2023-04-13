c7f32f2a9070e07fcee1c4c203942ca3d60dbe7f
Nisarga Adhikary <git@nisarga.me>
2023-04-14 00:59:50 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,9 +1,13 @@
 const YEAR = new Date().getFullYear()
+const now = new Date();
+const HOUR = now.getHours();
+const MINUTE = now.getMinutes();
+const SECOND = now.getSeconds();
 
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Nisarga Adhikary.
+      <time>{YEAR}</time> © Nisarga Adhikary | <date> {HOUR} : {MINUTE} : {SECOND} 📍 New Delhi, India </date>
      <a href="https://github.com/ni5arga/nisarga.me/">Source Code</a>
       <style jsx>{`
         a {
