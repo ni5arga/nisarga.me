96662dabdd8857891a3d35d7884c550642be52b6
Nisarga Adhikary <git@nisarga.me>
2023-09-17 20:33:36 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,10 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: "Nisarga's Website",
-    description: "Nisarga Adhikary's personal site",
-    image: 'https://c4.wallpaperflare.com/wallpaper/131/492/954/kimi-no-na-wa-makoto-shinkai-starry-night-comet-wallpaper-preview.jpg'
+    title: 'usr/bin/ni5arga~$',
+    description: 'Nisarga\'s personal site.',
+    image: 'https://i.pinimg.com/originals/f5/f2/74/f5f27448c036af645c27467c789ad759.gif'
   }
+
   return (
     <Html lang="en">
       <Head>
@@ -15,6 +16,8 @@ export default function Document() {
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@ni5arga" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
@@ -25,4 +28,3 @@ export default function Document() {
       </body>
     </Html>
   )
-}
