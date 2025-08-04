f4075e31f623565db2a812ea3125b81871c76bd7
nisarga <git@nisarga.me>
2025-08-05 01:45:59 +0530

    SEO enhancement attempt

--- pages/_document.js
+++ pages/_document.js
@@ -1,31 +1,65 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   const meta = {
     title: 'Nisarga',
-    description: 'personal site :)',
-    image: 'https://cdn.pixabay.com/photo/2022/12/01/04/40/backpacker-7628303_1280.jpg'
-  }
+    description: "ni5arga's little personal portfolio",
+    url: 'https://nisarga.me',
+    author: 'Nisarga Adhikary',
+    keywords: 'nisarga, ni5arga, nisarga adhikary, nisarga portfolio, ni5arga site, software engineer, CTF, open source'
+  };
+
+  const structuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Nisarga Adhikary',
+    url: meta.url,
+    sameAs: [
+      'https://github.com/ni5arga',
+      'https://twitter.com/ni5arga',
+      'https://www.linkedin.com/in/ni5arga/'
+    ],
+    jobTitle: 'Software Engineer',
+    worksFor: {
+      '@type': 'Organization',
+      name: 'Independent'
+    }
+  };
 
   return (
     <Html lang="en">
       <Head>
-        <meta name="robots" content="follow, index" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content={meta.author} />
         <meta name="description" content={meta.description} />
+        <meta name="keywords" content={meta.keywords} />
+
+        <meta property="og:type" content="website" />
         <meta property="og:site_name" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:url" content={meta.url} />
+
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@ni5arga" />
+        <meta name="twitter:creator" content="@ni5arga" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href={meta.url} />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
